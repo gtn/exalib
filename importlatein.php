72d16496b4618c0bf4943e86f581313cb3b182e9
Dietmar Angerer <dangerer@gtn-solutions.com>
2014-11-07 09:15:31 +0100

    new tables in customer import

--- importlatein.php
+++ importlatein.php
@@ -21,6 +21,8 @@ function block_exalib_importlatein() {
 	$data->id=568;$data->parent_id=0;
 	$DB->update_record('exalib_category', $data);
 	
+	$last_recid=$DB->insert_record("exalib_category", array("parent_id"=>0,"name"=>"Archiv"));
+	$newsletter_catid=$DB->insert_record("exalib_category", array("parent_id"=>$last_recid,"name"=>"Newsletter"));
 	
 	$items=$DB->get_records_sql("Select a.*,k.kategorie_tree_id as treeid from community_artikel as a INNER JOIN community_kategorie as k ON a.artikel_id = k.is_id");
 	foreach ($items as $item) {
@@ -57,6 +59,56 @@ function block_exalib_importlatein() {
 	//latein plattform als root ohne parentid setzen
 	$data->id=568;$data->parent_id=0;
 	$DB->update_record('exalib_category', $data);
+	
+	//community_html
+	
+	$htmls=$DB->get_records_sql("SELECT * FROM community_html");
+
+	foreach ($htmls as $html) {
+		$last_recid=$DB->insert_record("exalib_item", array("resource_id"=>0,"link" =>block_exalib_ersnull($html->imageurl),"source" =>  "","file" =>  "","name" =>  "html_comunity_rs","authors" =>  block_exalib_ersnull($html->autor),"content" =>block_exalib_ersnull($html->beschreibung)));
+		if (intval($last_recid)>0){
+			$DB->insert_record("exalib_item_category", array("item_id"=>$last_recid,"category_id" =>  $html->bereich));
+			//$sql='INSERT INTO {exalib_item_category} (item_id,category_id) VALUES ';
+			//$sql.='('.$last_recid.','.$html->bereich.')';
+			//$DB->Execute($sql);
+		}
+	}
+	//community_news
+	
+	$news=$DB->get_records_sql("SELECT * FROM community_news WHERE del=0");
+
+	foreach ($news as $new) {
+		$last_recid=$DB->insert_record("exalib_item", array("resource_id"=>0,"link" =>block_exalib_ersnull($new->url),"source" =>  "","file" =>  block_exalib_ersnull($new->imageurl),"name" =>$new->titel,"authors" =>  block_exalib_ersnull($new->autor),"content" =>"<div class='news_short'>".$new->short."</div>".$new->beschreibung));
+		if (intval($last_recid)>0){
+			$DB->insert_record("exalib_item_category", array("item_id"=>$last_recid,"category_id" =>  $new->bereich));
+			//$sql='INSERT INTO {exalib_item_category} (item_id,category_id) VALUES ';
+			//$sql.='('.$last_recid.','.$html->bereich.')';
+			//$DB->Execute($sql);
+		}
+	}
+	//community_newsletter
+	
+	$newsltrs=$DB->get_records_sql("SELECT * FROM community_newsletter");
+
+	foreach ($newsltrs as $newsltr) {
+		$data=array("resource_id"=>0,
+		"link" =>"",
+		"source" => "",
+		"file" =>block_exalib_ersnull($newsltr->attach_link),
+		"name" =>block_exalib_ersnull($newsltr->betreff),
+		"authors" =>  block_exalib_ersnull($newsltr->u_owner),
+		"content" =>"<div class='newsletter_inhalt'>".block_exalib_ersnull($newsltr->inhalt)."</div>Die Newsletterempfänger:<br><br>".block_exalib_ersnull($newsltr->recipients));
+		
+		$last_recid=$DB->insert_record("exalib_item", $data);
+		if (intval($last_recid)>0){
+			$DB->insert_record("exalib_item_category", array("item_id"=>$last_recid,"category_id" =>  $newsletter_catid));
+			//$sql='INSERT INTO {exalib_item_category} (item_id,category_id) VALUES ';
+			//$sql.='('.$last_recid.','.$html->bereich.')';
+			//$DB->Execute($sql);
+		}
+	}
+	
+	
 	
 }
 function block_exalib_ersnull($wert){
