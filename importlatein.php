6c3865f9ea667aefbc1c0cc4ddf403342eb5a845
Dietmar Angerer <dangerer@gtn-solutions.com>
2014-11-21 11:55:08 +0100

    new import fields

--- importlatein.php
+++ importlatein.php
@@ -26,32 +26,46 @@ function block_exalib_importlatein() {
 	
 	$items=$DB->get_records_sql("Select a.*,k.kategorie_tree_id as treeid from community_artikel as a INNER JOIN community_kategorie as k ON a.artikel_id = k.is_id");
 	foreach ($items as $item) {
-		$sql="INSERT INTO mdl_exalib_item (id,resource_id,link,source,file,name,authors,content) VALUES ";
-		$sql.="(".$item->artikel_id.",0,'','','','','','')";
-		$DB->Execute($sql);
-		
-		
-		$data= new stdClass();
-		$data->id=$item->artikel_id;$data->content=block_exalib_ersnull($item->inhalt);
-		$data->resource_id=block_exalib_ersnull($item->dokumentart_id);$data->link=block_exalib_ersnull($item->url_id);
-		$data->source=block_exalib_ersnull($item->quelle);$data->file=block_exalib_ersnull($item->bildlink);
-		$data->name=block_exalib_ersnull($item->titel);$data->authors=block_exalib_ersnull($item->autor);
-		$DB->update_record('exalib_item', $data);
-		
-		
-		$sql='INSERT INTO {exalib_item_category} (item_id,category_id) VALUES ';
-		$sql.='('.$item->artikel_id.','.$item->treeid.')';
-		$DB->Execute($sql);
-		//$DB->insert_record("exalib_category", array("id" =>  $category->kategorie_tree_id,"parent_id" =>  $category->kategorie_tree_high,"name" =>  $category->name));
+		if ($item->_delete==0){
+			$sql="INSERT INTO mdl_exalib_item (id,resource_id,link,source,file,name,authors,content) VALUES ";
+			$sql.="(".$item->artikel_id.",0,'','','','','','')";
+			$DB->Execute($sql);
+			
+			
+			$data= new stdClass();
+			$data->id=$item->artikel_id;$data->content=block_exalib_ersnull($item->inhalt);
+			$data->resource_id=block_exalib_ersnull($item->dokumentart_id);$data->link=block_exalib_ersnull($item->url_id);
+			$data->source=block_exalib_ersnull($item->quelle);$data->file=block_exalib_ersnull($item->bildlink);
+			$data->name=block_exalib_ersnull($item->titel);$data->authors=block_exalib_ersnull($item->autor);
+			$datum = new DateTime($item->erscheindatum);
+			$data->online_from=$datum->getTimestamp();
+			$data->time_created=$data->online_from;
+			$datum = new DateTime($item->verfallsdatum);
+			$data->online_to=$datum->getTimestamp();
+			
+			$data->hidden=block_exalib_inverthidden($item->genehmigt);
+						$datum = new DateTime($item->aenderungsdatum);
+			$data->time_modified=$datum->getTimestamp();
+			$data->modifiedby=$item->lastmodby;
+			
+			$DB->update_record('exalib_item', $data);
+			
+			
+			$sql='INSERT INTO {exalib_item_category} (item_id,category_id) VALUES ';
+			$sql.='('.$item->artikel_id.','.$item->treeid.')';
+			$DB->Execute($sql);
+			//$DB->insert_record("exalib_category", array("id" =>  $category->kategorie_tree_id,"parent_id" =>  $category->kategorie_tree_high,"name" =>  $category->name));
+		}
 	}
 	
-	$urls=$DB->get_records_sql("SELECT community_artikel.artikel_id,url.url_ID,url.url FROM url INNER JOIN community_artikel ON community_artikel.url_ID=url.url_ID");
+	$urls=$DB->get_records_sql("SELECT community_artikel.artikel_id,url.url_ID,url.url,url.url_titel FROM url INNER JOIN community_artikel ON community_artikel.url_ID=url.url_ID");
 	
 	foreach ($urls as $url) {
 		
 		if(!empty($url->url)){
 			$data= new stdClass();
 			$data->id=$url->artikel_id;$data->link=$url->url;
+			$data->link_titel=block_exalib_ersnull($url->url_titel);
 			$DB->update_record('exalib_item', $data);
 		}
 	}
@@ -111,8 +125,14 @@ function block_exalib_importlatein() {
 	
 	
 }
+function block_exalib_inverthidden($wert){
+	/*if genehmigt ($wert=1) return hidden=0*/
+	if ($wert==1) return 0;
+	else return 1;
+}
 function block_exalib_ersnull($wert){
 	if(empty($wert)) $wert="";
+	//if(is_null($wert)) $wert="";
 	return $wert;
 }
 
