1a36dd84b7c48a0574238780c8518be59502d80d
dprieler <dprieler@gmail.com>
2015-01-25 13:58:57 +0800

    latein version

--- importlatein.php
+++ importlatein.php
@@ -1,31 +1,26 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-//
-// Moodle is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// Moodle is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+require('inc.php');
 
-/**  
- * importlatein.php
- * @package    block_exalib
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @copyright  gtn gmbh <office@gtn-solutions.com>
- * @author       Daniel Prieler <dprieler@gtn-solutions.com>
- */
+block_exalib_require_admin();
 
-/**
- * block exalib importlatein urls
- * @return block
- */
+$importlatein = optional_param('importlatein', '0', PARAM_TEXT);
+
+if ($importlatein == '1') {
+	block_exalib_importlatein();
+}
+if ($importlatein == 'urls') {
+	block_exalib_importlatein_urls();
+}
+if ($importlatein == '2') {
+	block_exalib_importlatein2();
+}
+if ($importlatein == '3') {
+	block_exalib_importlatein3();
+}
+
+die('done');
+
+
 function block_exalib_importlatein_urls() {
 	global $DB;
 	
