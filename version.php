1ede10c5e8bfe926f3d391470dcee1bae26bc4ba
Dietmar Angerer <uab.katzinger@gmail.com>
2022-01-25 21:30:17 +0100

    fix bug message_send another place

--- version.php
+++ version.php
@@ -20,7 +20,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release = '4.6.0';
-$plugin->version   = 2022011402;      // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2022012200;      // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2014051200;    // Requires this Moodle version
 $plugin->component = 'block_exalib'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_STABLE; // MATURITY_STABLE.
